1fdd4043d3c011a1fd82b438cadd825f98099586
OleksandrAAA <aleksandkolesnik73@gmail.com>
2023-09-11 17:09:09 +0700

    Modified to show marketCap digit and img hover effect.

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,3 +1,23 @@
 $(document).ready(function() {
   /* Add custom javascript code here */
 });
+
+function numberToCurrencyWithUnit(inputString) {
+
+  const resultString = inputString.replace(/,/g, '');
+  const number = parseFloat(resultString);
+
+  if (isNaN(number))
+    return "";
+
+  if (number >= 1000000000)
+    return (number / 1000000000).toFixed(2) + "B";
+  
+  if (number >= 1000000)
+    return (number / 1000000).toFixed(2) + "M";
+
+  if (number >= 1000)
+    return (number / 1000).toFixed(2) + "K";
+
+  return number.toString();
+}
